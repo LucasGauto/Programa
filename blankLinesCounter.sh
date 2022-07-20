de2ad18884da45437f8e29b039ec82dbc94ae68c
Lucas Gauto <lucasgauto55555@gmail.com>
2022-07-19 21:02:15 -0300

    blankLinesCounter y findNames TERMINADOS

---
+++ blankLinesCounter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TEXTO=$1
+
+newIFS=$IFS
+
+IFS=$'\n'
+
+COUNT=$((0))
+
+for i in $(cat $TEXTO)
+do
+	if [ $i = " " ]
+	then
+		COUNT=$((COUNT+1))
+	fi
+done
+
+echo "Hay un total de $COUNT lineas en blanco"
+
+IFS=$newIFS
+
+unset newIFS
