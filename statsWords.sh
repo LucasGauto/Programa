84653429c938c97652559132604d30ed846f4746
Lucas Gauto <lucasgauto55555@gmail.com>
2022-07-27 10:17:48 -0300

    statsWords TERMINADO

--- statsWords.sh
+++ statsWords.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
-archivo=$1
-if [[ -e $archivo ]]
-then
-	for i in $(cat $archivo)
-	do
-		echo $i
-	done
-else
-	echo error
-fi
 
+TEXTO=$1
+LONGITUDES=$((0))
+CANTIDAD=$((0))
+
+for i in $(cat $TEXTO)
+do
+	LONGITUD=${#i}
+	
+	echo "$LONGITUD - $i" >> ordenar.txt
+	
+	LONGITUDES=$((LONGITUDES+$LONGITUD))
+	CANTIDAD=$((CANTIDAD+1))
+done
+
+ORDENADO=$(cat ordenar.txt | sort -h)
+
+N=$((1))
+
+newIFS=$IFS
+IFS=$'\n'
+
+for i in $ORDENADO
+do
+	if [ $N -eq 1 ]
+	then
+		echo "La palabra mas corta es: $i"
+		N=$((N + 1))
+	elif [ $N -eq $CANTIDAD ]
+	then
+		echo "La palabra mas larga es: $i"
+		N=$((N + 1))
+	else
+		N=$((N+1))
+	fi
+done
+
+IFS=$newIFS
+unset newIFS
+
+#---- promedio -----
+
+PROMEDIO=$(($LONGITUDES/$CANTIDAD))
+
+echo "Total de longitudes de las palabras: $LONGITUDES"
+echo "Cantidad total de palabras: $CANTIDAD"
+
+echo "El promedio de longitudes es de $PROMEDIO"
+
+rm ordenar.txt
