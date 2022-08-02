a6d886d2e4247efa760f9889544c7999497752d9
Lucas Gauto <lucasgauto55555@gmail.com>
2022-08-02 18:43:11 -0300

    substringReplace TERMINADO

--- substringReplace.sh
+++ substringReplace.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+function substringReplace {
+	TEXTO=$1
+
+	read -p "Ingrese la cadena que desea reemplazar: " cadena
+	read -p "Ingrese la cadena por la cual desea reemplazar la anterior: " cadena2
+
+	CAT=$(cat $TEXTO | sed "y/áéíóú/aeiou/" | sed "s/$cadena/$cadena2/g")
+
+	echo $CAT
+}
+
+substringReplace $1
